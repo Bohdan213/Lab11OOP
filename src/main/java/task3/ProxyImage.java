8d0879e1a70ddb2624e09ba04ecad83c57b75100
Bohdan213 <bogdanpeleh213@gmail.com>
2022-12-11 16:09:15 +0200

    task3 added

---
+++ src/main/java/task3/ProxyImage.java
@@ -0,0 +1,17 @@
+package task3;
+
+public class ProxyImage implements MyImage{
+    private final String filename;
+    private RealImage realImage;
+
+    public ProxyImage(String filename) {
+        this.filename = filename;
+    }
+
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(this.filename);
+        }
+        realImage.display();
+    }
+}
